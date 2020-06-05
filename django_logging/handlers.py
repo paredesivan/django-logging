b7921a2ba66c2367b5ddfb816e0232f040c58f7c
Matthew Pecarina <mattp@Matthews-MacBook-Pro.local>
2020-06-05 16:18:05 -0500

    update django console logger

--- django_logging/handlers.py
+++ django_logging/handlers.py
@@ -95,7 +95,8 @@ class ConsoleHandler(StreamHandler):
     def format(self, record):
         if isinstance(record.msg, LogObject) or isinstance(record.msg, SqlLogObject):
             created = int(record.created)
-            message = {'severity': record.levelname, 'timestamp': datetime.datetime.fromtimestamp(created).isoformat(), record.msg.to_dict}
+            msg_dict = record.msg.to_dict
+            message = {'severity': record.levelname, 'timestamp': datetime.datetime.fromtimestamp(created).isoformat(), msg_dict}
 
             try:
                 indent = int(settings.INDENT_CONSOLE_LOG)
@@ -106,7 +107,8 @@ class ConsoleHandler(StreamHandler):
             return str(record.msg)
         elif isinstance(record.msg, dict):
             created = int(record.created)
-            message = {'severity': record.levelname, 'timestamp': created, record.msg.to_dict}
+            msg_dict = record.msg.to_dict
+            message = {'severity': record.levelname, 'timestamp': created, msg_dict}
             return json.dumps(message, separators=(',', ':'))
         else:
             return super(ConsoleHandler, self).format(record)
