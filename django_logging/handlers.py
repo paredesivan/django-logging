7dd20e32401903be1cfcbbf96d250242431f210f
Matthew Pecarina <mattp@Matthews-MacBook-Pro.local>
2020-06-05 12:29:52 -0500

    update logger to not format json

--- django_logging/handlers.py
+++ django_logging/handlers.py
@@ -101,9 +101,7 @@ class ConsoleHandler(StreamHandler):
                 indent = int(settings.INDENT_CONSOLE_LOG)
             except (ValueError, TypeError):
                 indent = 1
-            import pprint
-            message = pprint.pformat(message, indent, 160, compact=True)
-            return message
+            return json.dumps(message, separators=(',', ':'))
         elif isinstance(record.msg, ErrorLogObject):
             return str(record.msg)
         elif isinstance(record.msg, dict):
