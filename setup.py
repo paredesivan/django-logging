c6652131acc9c1770e9c513d1c9109c404db3f4c
Matthew Pecarina <mattp@Matthews-MacBook-Pro.local>
2020-06-12 01:03:21 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -3,29 +3,16 @@ import re
 from setuptools import setup, find_packages
 
 
-def readme():
-    with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-        return readme.read()
-
-
-def version():
-    pattern = re.compile(r'__version__ = \'([\d\.]+)\'')
-    with open(os.path.join('django_logging', '__init__.py')) as f:
-        data = f.read()
-        return re.search(pattern, data).group(1)
-
-
 setup(
-    name='django-logging-json',
-    version=version(),
+    name='django-logging-json-fork',
+    version="0.0.0",
     packages=find_packages(),
     include_package_data=True,
     license='BSD',
-    description='A simple Django app to log requests/responses in various formats, such as JSON.',
-    long_description=readme(),
-    url='https://github.com/cipriantarta/django-logging',
-    author='Ciprian Tarta',
-    author_email='me@cipriantarta.ro',
+    description='Fork update of cipriantarta/django-logging.',
+    url='https://github.com/mpecarina/django-logging',
+    author='Matthew Pecarina',
+    author_email='mattp@hbci.com',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
